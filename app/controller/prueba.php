df7cd483b94c40835abd6bf300545fb17b60cbe4
XxEduardo032xX <153869902+XxEduardo032xX@users.noreply.github.com>
2024-04-16 00:00:35 -0500

    cacasdasdasd

--- app/controller/prueba.php
+++ app/controller/prueba.php
@@ -2,7 +2,7 @@
 class ControllerPrueba{
 
     public static function getPrueba(){
-        //Debes de colocar un return para que te devuelva los resultados en la funcion GETPERSON:
+        //Debes de colocar un return para que te devuelva los resultados en la funcion getPrueba:
         return ModelPrueba::getPrueba();
     }
 
