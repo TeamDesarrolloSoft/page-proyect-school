ce85782f168fa2d35b1eec52604abcb015f88a2b
kCFSMp <cesarsaca04@gmail.com>
2024-05-02 01:56:30 -0500

    bot agregado a view/index.php

--- public/view/index.php
+++ public/view/index.php
@@ -5,7 +5,11 @@ $fileCSS = ["home-page", "global"];
 
 require_once("./public/include/html_head.php");
 ?>
-
+<head><!-- BOT -->
+    <script src="https://kit.fontawesome.com/75c41b6d39.js" crossorigin="anonymous"></script> 
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+    <link rel="stylesheet" href="public\css\chatbot.css">
+</head>
 <body class="bg-black-low">
 
   <nav class="bg-primary">
@@ -88,7 +92,7 @@ require_once("./public/include/html_head.php");
                 <label for="email">Tu correo electrónico:</label>
                 <input type="email" id="email" name="email" placeholder="Tu correo electrónico" required>
             </div>
-            <button type="submit">Enviar</button>
+            <button class="ff"type="submit">Enviar</button>
         </form>
     </div>
 </section>
@@ -96,7 +100,71 @@ require_once("./public/include/html_head.php");
   <footer class="bg-primary">
     <p class="c-white">&copy; 2024 Colegio Pepito Perez. Todos los derechos reservados.</p>
   </footer>
+<!-- BOT -->
 
+<button id="toggleButton"><i class="fa-solid fa-robot"></i></button>
+    <div id="burbuja">
+        <div class="wrapper">
+            <div class="title">ChatBot "Pepito Perez"</div>
+            <div class="form">
+                <div class="bot-inbox inbox">
+                    <div class="icon">
+                        <i class="fas fa-user"></i>
+                    </div>
+                    <div class="msg-header">
+                        <p>Hola, ¿cómo puedo ayudarte?</p>
+                    </div>
+                </div>
+            </div>
+            <div class="typing-field">
+                <div class="input-data">
+                    <input id="data" type="text" placeholder="Escribe algo aquí.." required>
+                    <button id="send-btn">Enviar</button>
+                </div>
+            </div>
+        </div>
+    </div>
+    <script>
+    $(document).ready(function() {
+            $("#send-btn").on("click", function() {
+                $value = $("#data").val();
+                $msg = '<div class="user-inbox inbox"><div class="msg-header"><p>' + $value + '</p></div></div>';
+                $(".form").append($msg);
+                $("#data").val('');
+
+                // iniciar el código ajax
+                $.ajax({
+                    url: './app/trigger/chatbot.php',
+                    type: 'POST',
+                    data: 'text=' + $value,
+                    success: function(result) {
+                        $replay = '<div class="bot-inbox inbox"><div class="icon"><i class="fas fa-user"></i></div><div class="msg-header"><p>' + result + '</p></div></div>';
+                        $(".form").append($replay);
+                        // cuando el chat baja, la barra de desplazamiento llega automáticamente al final
+                        $(".form").scrollTop($(".form")[0].scrollHeight);
+                    }
+                });
+            });
+        });
+
+    document.getElementById("data").addEventListener("keyup", function(event) {
+        if (event.keyCode === 13) { 
+            event.preventDefault();
+            document.getElementById("send-btn").click();
+        }
+    });
+
+    document.getElementById("toggleButton").addEventListener("click", function() {
+        var div = document.getElementById("burbuja");
+        if (div.style.display === "none" || div.style.display === "") {
+            div.style.display = "block"; 
+            div.style.position = "fixed"; // Ajusta la posición al mostrar
+            div.style.zIndex = "9999"; // Ajusta el z-index al mostrar
+        } else {
+            div.style.display = "none";
+        }
+    });
+</script>
 </body>
 
 </html>
