c13390efe7caa344a654360bf52c52ad91de4fe4
Diego Levano <diegolevanososa@gmail.com>
2024-05-01 21:24:49 -0500

    sin bootstrap

--- public/includeLogueoAdmin/html_head.php
+++ public/includeLogueoAdmin/html_head.php
@@ -12,20 +12,13 @@
 
 
     <!-- Css de la nube, osea cdn: -->
-    <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet"
-    integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous"> --> 
-    
 
     <!-- Si hay valores en $fileCSS, lo recorrera y colocara cada valor en un en enlace de abajo -->
     <?php foreach($fileCSS as $key => $filename) :?>
         <link rel="stylesheet" href="../css/<?= str_replace(".", "/", $filename) ;?>.css">
     <?php endforeach ?>
-
-
     
     <!-- JS NUBE -->
-    <!-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-    <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script> -->
     <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
     
     <!-- js local -->
